3442472481b88af331edec86078ced19f094cbfc
Alex Knight <alex@alexknight.co.uk>
2022-02-10 08:12:31 +0000

    Moved api function helpers/utilities out of class + minor changes
    - Added loading text to Login to avoid linting error
    - Ran `pod install` on ios folder.

--- src/services/utils/SpacebookClient.ts
+++ src/services/utils/SpacebookClient.ts
@@ -31,88 +31,85 @@ function ok<T, Err>(result: T): Handler<T, Err> {
   return {intendedResult: result};
 }
 
-export class SpacebookClient {
-  private static baseURL: string = 'http://localhost:3333/api/1.0.0/';
+const baseURL: string = 'http://localhost:3333/api/1.0.0/';
 
-  private static queryBuilder(
-    input: Array<{key: string; value: string}> | undefined,
-  ): string {
-    if (input === undefined || input?.length === 0) {
-      return '';
-    } else {
-      const endTargetIndex = input.length - 1;
-      return input.reduce((acc, value, index) => {
-        acc += `${value.key}=${value.value}`;
-        if (index !== endTargetIndex) {
-          acc += '&';
-        }
-        return acc;
-      }, '?');
-    }
-  }
-
-  private static async authKey(): Promise<
-    string | CommonAppErrors.TokenNotFound
-  > {
-    return Keychain.getGenericPassword()
-      .then(store => {
-        if (store) {
-          return store.password;
-        } else {
-          return CommonAppErrors.TokenNotFound;
-        }
-      })
-      .catch(error => {
-        console.error(error);
-        return CommonAppErrors.TokenNotFound;
-      });
-  }
-
-  private static async req<RequestT extends object | undefined = undefined>(
-    url: string,
-    verb: keyof typeof Verbs,
-    requestBody: RequestT,
-    parameterQueries?: Array<{key: string; value: string}>,
-    requriesAuth: boolean = true,
-  ): Promise<Request> {
-    const pathQueries: string = SpacebookClient.queryBuilder(parameterQueries);
-
-    const fullURL: string = `${this.baseURL}${url}${pathQueries}`;
-
-    const headersToInclude = new Headers({'Content-Type': 'application/json'});
-
-    if (requriesAuth) {
-      const potentialKey: string | CommonAppErrors.TokenNotFound =
-        await this.authKey();
-
-      if (potentialKey === CommonAppErrors.TokenNotFound) {
-        console.error(
-          'Token not found for authenticating requests! Everything will crash & burn',
-        );
-        return Promise.reject(
-          `API request ${url} requires auth but error whilst obtaining token`,
-        );
-      } else {
-        headersToInclude.set('X-Authorization', potentialKey);
+function queryBuilder(
+  input: Array<{key: string; value: string}> | undefined,
+): string {
+  if (input === undefined || input?.length === 0) {
+    return '';
+  } else {
+    const endTargetIndex = input.length - 1;
+    return input.reduce((acc, value, index) => {
+      acc += `${value.key}=${value.value}`;
+      if (index !== endTargetIndex) {
+        acc += '&';
       }
+      return acc;
+    }, '?');
+  }
+}
+
+function authKey(): Promise<string | CommonAppErrors.TokenNotFound> {
+  return Keychain.getGenericPassword()
+    .then(store => {
+      if (store) {
+        return store.password;
+      } else {
+        return CommonAppErrors.TokenNotFound;
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      return CommonAppErrors.TokenNotFound;
+    });
+}
+
+export async function req<RequestT extends object | undefined = undefined>(
+  url: string,
+  verb: keyof typeof Verbs,
+  requestBody: RequestT,
+  parameterQueries?: Array<{key: string; value: string}>,
+  requriesAuth: boolean = true,
+): Promise<Request> {
+  const pathQueries: string = queryBuilder(parameterQueries);
+
+  const fullURL: string = `${baseURL}${url}${pathQueries}`;
+
+  const headersToInclude = new Headers({'Content-Type': 'application/json'});
+
+  if (requriesAuth) {
+    const potentialKey: string | CommonAppErrors.TokenNotFound =
+      await authKey();
+
+    if (potentialKey === CommonAppErrors.TokenNotFound) {
+      console.error(
+        'Token not found for authenticating requests! Everything will crash & burn',
+      );
+      return Promise.reject(
+        `API request ${url} requires auth but error whilst obtaining token`,
+      );
+    } else {
+      headersToInclude.set('X-Authorization', potentialKey);
     }
-
-    const finalRequestDetails = {
-      method: verb,
-      headers: headersToInclude,
-      body: JSON.stringify(requestBody),
-    } as RequestInit;
-
-    console.log(fullURL);
-    console.log(requriesAuth);
-    return new Request(fullURL, finalRequestDetails);
   }
 
+  const finalRequestDetails = {
+    method: verb,
+    headers: headersToInclude,
+    body: JSON.stringify(requestBody),
+  } as RequestInit;
+
+  console.log(fullURL);
+  return new Request(fullURL, finalRequestDetails);
+}
+
+export class SpacebookClient {
   static async login(
     username: string,
     password: string,
   ): Promise<Handler<LoginResponse, LoginError>> {
-    const testRequest: Request = await SpacebookClient.req<LoginRequest>(
+    const testRequest: Request = await req<LoginRequest>(
       'login',
       Verbs.POST,
       {
@@ -162,7 +159,7 @@ export class SpacebookClient {
     last_name: string,
     password: string,
   ): Promise<Handler<RegisterResponse, RegisterErrors>> {
-    const testRequest = await SpacebookClient.req<RegisterRequest>(
+    const testRequest = await req<RegisterRequest>(
       'user',
       Verbs.POST,
       {
@@ -208,7 +205,7 @@ export class SpacebookClient {
   }
 
   static async logout(): Promise<Handler<Success, LogoutError>> {
-    const request = await this.req('logout', Verbs.POST, {});
+    const request = await req('logout', Verbs.POST, {});
 
     return fetch(request)
       .then(async (response: Response) => {
