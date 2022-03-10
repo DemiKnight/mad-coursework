e2c7cc8a0afe93c7d12990dc067a18fa0244a4d7
Alex Knight <alex@alexknight.co.uk>
2022-03-10 23:55:04 +0000

    Fixed post list

--- src/components/Posts/PostList/PostList.tsx
+++ src/components/Posts/PostList/PostList.tsx
@@ -1,35 +1,49 @@
 import React from 'react';
 import {Post, PublicUser} from '../../../services/utils/SpacebookRequests';
-import {SafeAreaView, View, VirtualizedList} from 'react-native';
+import {StyleSheet, View, VirtualizedList} from 'react-native';
 import Text from 'react-native-elements/dist/text/Text';
 import {UserPost} from '../Post/UserPost';
+import {getAllPosts} from '../../../api/Posting';
+import {mapErrors} from '../../../api/RequestUtils';
 
 export const PostList = (props: {userId: number}) => {
-  const [postList, setPostList] = React.useState<Array<Post>>([
-    {
-      post_id: 9,
-      text: 'Post for me plz',
-      timestamp: '2022-02-18T14:20:29.000Z',
-      author: {
-        user_id: 1,
-        user_givenname: 'Alex',
-        user_familyname: 'Knight',
-        user_email: 'alex@alexknight.co.uk',
-      },
-      numLikes: 0,
-    },
-  ]);
-  // React.useEffect(() => {});
+  const [postList, setPostList] = React.useState<Array<Post>>([]);
+  const [errors, setErrors] = React.useState<Array<string>>([]);
+
+  const getData = React.useCallback(async () => {
+    const result = await getAllPosts(props.userId);
+
+    if (result.intendedResult !== undefined) {
+      setPostList(result.intendedResult);
+    } else {
+      setErrors(mapErrors(result.errors, 'Getting', ''));
+    }
+  }, [props.userId]);
+
+  React.useEffect(() => {
+    getData();
+  }, [getData]);
 
   return (
-    <SafeAreaView>
-      <VirtualizedList<Post>
-        renderItem={item => <UserPost post={item.item} />}
-        getItem={(data: Array<Post>, index) => data[index]}
-        getItemCount={x => x.length}
-        keyExtractor={(item, _) => String(item.post_id)}
-        data={postList}
-      />
-    </SafeAreaView>
+    <VirtualizedList<Post>
+      renderItem={item => (
+        <>
+          <Text>{item.index}</Text>
+          <UserPost post={item.item} />
+        </>
+      )}
+      getItem={(data: Array<Post>, index) => data[index]}
+      getItemCount={x => x.length}
+      keyExtractor={(item, _) => String(item.post_id)}
+      data={postList}
+    />
   );
 };
+
+const styles = StyleSheet.create({
+  postListWrapper: {
+    borderColor: '#008000',
+    borderWidth: 5,
+    maxHeight: 300,
+  },
+});
